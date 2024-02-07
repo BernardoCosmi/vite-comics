b52411d137953bf5d2d4b5d71c152271f83044ba
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-02-07 15:56:11 +0100

    file upload

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import AppHeader from "./components/AppHeader.vue"
+import AppBody from "./components/AppBody.vue"
+import AppLinks from "./components/AppLinks.vue"
+import AppFooter from "./components/AppFooter.vue"
+
+export default {
+  components: {
+    AppHeader,
+    AppBody,
+    AppLinks,
+    AppFooter,
+  },
+};
+</script>
+
+<template>
+    <AppHeader/>
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,57 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+
+</script>
+
+<template>
+  <div class="container">
+    <!-- LOGO -->
+    <div>
+      <img src="../assets/images/dc-logo.png" alt="Logo Dc Comix">
+    </div>
+
+    <!-- Menu -->
+    <div>
+      <ul>
+        <li>
+          <span>characters</span>
+        </li>
+        <li>
+          <span>comics</span>
+        </li>
+        <li>
+          <span>movies</span>
+        </li>
+        <li>
+          <span>tv</span>
+        </li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+    
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+@use "../styles/partials/mixins" as *;
+
+.container{
+  display: flex;
+  width: 80vw;
+  margin: auto;
+  justify-content: space-between;
+}
+ul{
+  list-style-type: none;
+  display: flex;
+}
+
+</style>
